abbfac0ba5dacc1c94eee05552016260d8f0d339
kaualima7 <kaualima2007@outlook.com>
2026-06-10 19:17:11 -0300

    classes e att da estrutura

---
+++ src/tecnico.py
@@ -0,0 +1,7 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+            self.nome = nome
+            self.idade = idade
+    
+    def apresentar(self):
+        return f"Nome: {self.nome}, Idade: {self.idade}"
