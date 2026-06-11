5690c93eee04cef29c06af5cc753d2ae1bf4bfdd
kaualima7 <kaualima2007@outlook.com>
2026-06-11 13:30:11 -0300

    ajuste main

--- src/main.py
+++ src/main.py
@@ -2,25 +2,158 @@ from jogador import Jogador
 from tecnico import Tecnico
 from clube import Clube
 from partida import Partida
+from campeonato import Campeonato
 
-t1 = Tecnico("Abel", 46, "Ofensiva", 15)
-t2 = Tecnico("Renato", 62, "Defensiva", 20)
 
-j1 = Jogador("Neymar", 33, 10, "Atacante")
-j2 = Jogador("Casemiro", 32, 5, "Volante")
-j3 = Jogador("Endrick", 19, 18, "Atacante")
+#Função pra criar lista dos jogadores
+def criar_jogadores(lista_nomes):
+    jogadores = []
 
-clube1 = Clube("Brasil", t1)
-clube2 = Clube("Gremio FBPA", t2)
+    for numero, nome in enumerate(lista_nomes, start=1):
+        jogadores.append(
+            Jogador(
+                nome,
+                25,
+                numero,
+                "Jogador"
+            )
+        )
+    return jogadores
 
-clube1.adicionar_jogador(j1)
-clube1.adicionar_jogador(j2)
+nomes_gremio = [
+    "Weverton",
+    "Pavon",
+    "Viery",
+    "Gustavo Martins",
+    "Marlon",
+    "Noriega",
+    "Leo Perez",
+    "Gabriel Mec",
+    "Enamorado",
+    "Carlos Vinicius",
+    "Amuzu"
+]
 
-clube2.adicionar_jogador(j3)
+nomes_brasil = [
+    "Alisson",
+    "Danilo",
+    "Marquinhos",
+    "Gabriel Magalhaes",
+    "Alex Sandro",
+    "Casemiro",
+    "Bruno Guimaraes",
+    "Neymar",
+    "Raphinha",
+    "Vinicius Junior",
+    "Rayan"
+]
 
-partida1 = Partida(clube1, clube2)
+print("===================================")
+print(" SISTEMA DA COPA AMISTOSA ")
+print("===================================")
+
+##############################################################
+# Técnicos
+print("\nCriando técnicos...")
+
+tecnico_gremio = Tecnico(
+    "Luís Castro",
+    64,
+    "Nem ele sabe",
+    30
+)
+
+tecnico_brasil = Tecnico(
+    "Carlo Ancelotti",
+    67,
+    "Equilibrada",
+    40
+)
+
+print(tecnico_gremio.apresentar())
+print(tecnico_brasil.apresentar())
+
+##############################################################
+# Clube/Seleção
+print("Criando clubes...")
+
+gremio = Clube(
+    "Gremio",
+    tecnico_gremio
+)
+
+brasil = Clube(
+    "Brasil",
+    tecnico_brasil
+)
+
+print("Criando jogadores...")
+
+jogadores_gremio = criar_jogadores(nomes_gremio)
+jogadores_brasil = criar_jogadores(nomes_brasil)
+
+print("Adicionando jogadores aos clubes...")
+
+for jogador in jogadores_gremio:
+    gremio.adicionar_jogador(jogador)
+
+for jogador in jogadores_brasil:
+    brasil.adicionar_jogador(jogador)
+
+print("\n=== ELENCOS ===")
+gremio.mostrar_elenco()
+brasil.mostrar_elenco()
+
+# Campeonato
+print("\nCriando campeonato...")
+
+campeonato = Campeonato("Copa Amistosa")
+
+campeonato.adicionar_time(gremio)
+campeonato.adicionar_time(brasil)
+
+print("\n=== ANTES DA PARTIDA ===")
+print(f"{gremio.nome}: {gremio.pontos} pontos")
+print(f"{brasil.nome}: {brasil.pontos} pontos")
+
+jogadores_gremio[9].fazer_gol()
+print(
+    f"\n{jogadores_gremio[9].nome} marcou um gol!"
+)
+
+jogadores_brasil[5].fazer_gol()
+print(
+    f"\n{jogadores_brasil[5].nome} marcou um gol"
+)
+
+jogadores_gremio[10].fazer_gol()
+print(
+    f"\n{jogadores_gremio[10].nome} marcou um gol"
+)
+
+# Partida
+print("\nRegistrando partida...")
+
+partida1 = Partida(gremio, brasil)
 
 partida1.registrar_resultado(2, 1)
 
-print(f"{clube1.nome}: {clube1.pontos} pontos")
-print(f"{clube2.nome}: {clube2.pontos} pontos")
+print("\n=== DEPOIS DA PARTIDA ===")
+print(f"{gremio.nome}: {gremio.pontos} pontos")
+print(f"{brasil.nome}: {brasil.pontos} pontos")
+
+campeonato.adicionar_partida(partida1)
+
+print("\nResultado:")
+print(f"{gremio.nome} 2 x 1 {brasil.nome}")
+
+# Tabela
+campeonato.mostrar_tabela()
+
+# Campeão
+campeao = campeonato.definir_campeao()
+
+print("\n=== CAMPEÃO ===")
+print(campeao.nome)
+print(f"Pontos: {campeao.pontos}")
+print(f"Saldo de gols: {campeao.saldo_gols()}")
